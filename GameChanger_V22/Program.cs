c09d1013fa91dd7da05238819e34975b755ac2d3
undugy <70964651+undugy@users.noreply.github.com>
2022-10-10 21:19:26 +0900

    ZLogger를 추가하라
    logging을 위해 nuget추가
    로그 파일이 너무 커지는 것을 방지하기위해 RollingFile설정함

--- GameChanger_V22/Program.cs
+++ GameChanger_V22/Program.cs
@@ -1,18 +1,28 @@
+using ZLogger;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Host.ConfigureLogging(logging =>
+{
+    logging.ClearProviders();
+    logging.AddConfiguration(builder.Configuration.GetSection("logging"));
+    logging.AddZLoggerConsole();
+    logging.AddZLoggerFile("DailyLog.log");
+    logging.AddZLoggerRollingFile((dt, x) =>
+        $"logs/{dt.ToLocalTime():yyyy-MM-dd}_{x:000}.log", x => x.ToLocalTime().Date, 1024);
+});
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+app.UseRouting();
 
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
